12bebd20fed3b43f55bda58cc52d59f75d657ebd
Cacofonix <box.safe@gmail.com>
2012-09-11 18:26:30 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bkngjobs::Application.config.secret_token = '8687afeccf28b4588e6f18084351a18120d24f61d425ec1ee9cde2c290bc8c598594edbb2a8568b33dea9f03350e4ce941840a28584e4cffa39ddeea77bbe8ba'
