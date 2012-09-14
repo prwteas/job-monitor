1afbe43317023cd5281f7a11ccee82ae033eb66f
Cacofonix <box.safe@gmail.com>
2012-09-14 10:44:59 +0200

    setup resque attempt 3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Bkngjobs::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6789'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
